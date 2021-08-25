a2fe0ad9fb6f69096e3f768792fd35d6085937a3
Shawn Truesdale <struesd@am.trimblecorp.net>
2021-08-25 11:38:28 -0600

    Correct eslint depend, integrate row virtualization

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 
 import TestTable from './components/testTable';
--- src/components/testTable.jsx
+++ src/components/testTable.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-debugger */
+/* eslint-disable react/jsx-key */
 import React, { useCallback, useMemo } from 'react';
 import clsx from 'clsx';
 import {
@@ -10,57 +12,56 @@ import {
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { useResizeColumns, useTable, useBlockLayout } from 'react-table';
-import { FixedSizeList } from 'react-window'
+import { FixedSizeList } from 'react-window';
 
+import scrollbarWidth from './scrollbarWidth';
 import dataInterface from '../data/dataInterface';
 import mockData from '../data/mockData.json';
 
-const useStyles = makeStyles((theme) => {
-    return {
-        shipmentsTableWrapper: {
-            width: '100%',
-            height: '90vh',
-            border: '2px solid rgba(0,0,0,.18)',
-            overflow: 'scroll',
-            display: 'inline-block',
+const useStyles = makeStyles((theme) => ({
+    shipmentsTableWrapper: {
+        width: '100%',
+        height: '90vh',
+        border: '2px solid rgba(0,0,0,.18)',
+        overflow: 'scroll',
+        display: 'inline-block',
+    },
+    tableHeader: {
+        fontSize: '12px',
+        lineHeight: '16px',
+        fontWeight: '600',
+    },
+    tableRow: {
+        '&:nth-of-type(odd)': {
+            backgroundColor: theme.palette.action.hover,
         },
-        tableHeader: {
-            fontSize: '12px',
-            lineHeight: '16px',
-            fontWeight: '600',
-        },
-        tableRow: {
-            '&:nth-of-type(odd)': {
-                backgroundColor: theme.palette.action.hover,
-            },
-        },
-        tableCell: {
-            fontSize: '12px',
-            whiteSpace: 'nowrap',
-            overflow: 'hidden',
-            textOverflow: 'ellipsis',
-            border: '1px solid rgba(0, 0, 0, 0.1)',
-            padding: '0.75rem',
-            // using absolute position resizer, this is needed to accurately space the column cells
-            position: 'relative',
-        },
-        resizer: {
-            display: 'inline-block',
-            background: 'grey',
-            width: '7px',
-            radius: '50%',
-            height: '100%',
-            position: 'absolute',
-            right: '0',
-            top: '0',
-            transform: 'translateX(50%)',
-            zIndex: '1',
-        },
-        isResizing: {
-            background: 'blue',
-        },
-    };
-});
+    },
+    tableCell: {
+        fontSize: '12px',
+        whiteSpace: 'nowrap',
+        overflow: 'hidden',
+        textOverflow: 'ellipsis',
+        border: '1px solid rgba(0, 0, 0, 0.1)',
+        padding: '0.75rem',
+        // using absolute position resizer, this is needed to accurately space the column cells
+        position: 'relative',
+    },
+    resizer: {
+        display: 'inline-block',
+        background: 'grey',
+        width: '7px',
+        radius: '50%',
+        height: '100%',
+        position: 'absolute',
+        right: '0',
+        top: '0',
+        transform: 'translateX(50%)',
+        zIndex: '1',
+    },
+    isResizing: {
+        background: 'blue',
+    },
+}));
 
 function TestTable() {
     const classes = useStyles();
@@ -77,11 +78,11 @@ function TestTable() {
             width: 125,
             maxWidth: 400,
         }),
-        []
+        [],
     );
 
     // used in our react-window virtuilization, this allows correct formatting for the table width
-    const scrollBarSize = useMemo(() => scrollbarWidth(), [])
+    const scrollBarSize = useMemo(() => scrollbarWidth(), []);
 
     // memoize the data so we aren't rerendering on table adjustments
     const columns = useMemo(
@@ -89,20 +90,19 @@ function TestTable() {
             {
                 Header: 'Shipment Info',
                 // only for PoC, so I don't have to write 20 explicit columns
-                columns: dataInterface.map((keyName) => {
-                    return {
-                        Header: keyName,
-                        accessor: keyName,
-                    };
-                }),
+                columns: dataInterface.map((keyName) => ({
+                    Header: keyName,
+                    accessor: keyName,
+                })),
             },
         ],
-        []
+        [],
     );
 
-    // use the functions returned from useTable to build the UI
+    // Use the functions returned from useTable to build the UI
     // useResizeColumns enables use of resizing columns, along with defaultColumn
-    // to define the initial sizing and spacing
+    //      to define the initial sizing and spacing
+    // totalColumnsWidth is necessary for using virtualization
     // This is also the place to add necessary hooks to use from react-table
     const {
         getTableProps,
@@ -110,6 +110,7 @@ function TestTable() {
         rows,
         prepareRow,
         getTableBodyProps,
+        totalColumnsWidth,
         resetResizing,
     } = useTable(
         {
@@ -118,15 +119,15 @@ function TestTable() {
             defaultColumn,
         },
         useBlockLayout,
-        useResizeColumns
+        useResizeColumns,
     );
 
     // Used for virtualizing our rows using react-window
     // Essentially this is what would originally be in our JSX render return
     const RenderRow = useCallback(
         ({ index, style }) => {
-            const row = rows[index]
-            prepareRow(row)
+            const row = rows[index];
+            prepareRow(row);
             return (
                 <div
                     {...row.getRowProps({
@@ -134,18 +135,16 @@ function TestTable() {
                     })}
                     className={classes.tableRow}
                 >
-                    {rows.cells.map((cell) => {
-                        return (
-                            <div {...cell.getCellProps()} className={classes.tableCell}>
-                                {cell.render('Cell')}
-                            </div>
-                        )
-                    })}
+                    {row.cells.map((cell) => (
+                        <div {...cell.getCellProps()} className={classes.tableCell}>
+                            {cell.render('Cell')}
+                        </div>
+                    ))}
                 </div>
-            )
+            );
         },
-        [prepareRow, rows]
-    )
+        [prepareRow, rows],
+    );
 
     return (
         <TableContainer className={classes.shipmentsTableWrapper}>
